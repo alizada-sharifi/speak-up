2274068cb30d761af9eaa38f93514cdd733bd1bf
Alizada Sharifi <alizadasharifi2022@gmail.com>
2026-03-07 09:44:33 +0430

    fix: resolve vercel errors

--- src/constants/layout.ts
+++ src/constants/layout.ts
@@ -1,37 +1,37 @@
-import { ROUTES } from "./route";
+import { ROUTES } from './route';
 
-export const Navbars: { label: string; value: string }[] = [
-  { label: "home", value: ROUTES.HOME },
-  { label: "courses", value: ROUTES.COURSES },
-  { label: "contact", value: ROUTES.CONTACT },
-  { label: "about", value: ROUTES.ABOUT },
-];
+export const Navbars = [
+  { label: 'home', value: ROUTES.HOME },
+  { label: 'courses', value: ROUTES.COURSES },
+  { label: 'contact', value: ROUTES.CONTACT },
+  { label: 'about', value: ROUTES.ABOUT },
+] as const;
 
 export const lnaguages: { flag: string; label: string; value: string }[] = [
-  { flag: "/images/US.svg", label: "English", value: "en" },
-  { flag: "/images/DE.svg", label: "German", value: "de" },
-  { flag: "/images/GP.svg", label: "France", value: "fr" },
-  { flag: "/images/TR.svg", label: "Turkiye", value: "tr" },
+  { flag: '/images/US.svg', label: 'English', value: 'en' },
+  { flag: '/images/DE.svg', label: 'German', value: 'de' },
+  { flag: '/images/GP.svg', label: 'France', value: 'fr' },
+  { flag: '/images/TR.svg', label: 'Turkiye', value: 'tr' },
 ];
 
 export const footerData = [
   {
-    title: "footer.courses",
+    title: 'footer.courses',
     values: [
-      { label: "footer.learnEnglish", value: "/" },
-      { label: "footer.learnFrench", value: "/" },
-      { label: "footer.learnTurkey", value: "/" },
-      { label: "footer.learnGerman", value: "/" },
+      { label: 'footer.learnEnglish', value: '/' },
+      { label: 'footer.learnFrench', value: '/' },
+      { label: 'footer.learnTurkey', value: '/' },
+      { label: 'footer.learnGerman', value: '/' },
     ],
   },
 
   {
-    title: "footer.products",
+    title: 'footer.products',
     values: [
-      { label: "navigation.landingPage", value: "/" },
-      { label: "navigation.about", value: "/" },
-      { label: "navigation.contact", value: "/" },
-      { label: "navigation.courses", value: "/" },
+      { label: 'navigation.landingPage', value: '/' },
+      { label: 'navigation.about', value: '/' },
+      { label: 'navigation.contact', value: '/' },
+      { label: 'navigation.courses', value: '/' },
     ],
   },
-];
+] as const;
